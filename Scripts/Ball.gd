f22ac7098f0d0a52a5490665d4bd716028d2c0cf
Soulzerz <Soulzerz@github.com>
2020-07-25 03:28:15 -0400

    Modified: Ball node to add velocity and speed

--- Scripts/Ball.gd
+++ Scripts/Ball.gd
@@ -1,5 +1,7 @@
 extends RigidBody2D
 
+const SPEEDUP = 400
+const MAXSPEED = 30000
 
 func _ready():
 	set_physics_process(true)
@@ -9,3 +11,13 @@ func _physics_process(delta):
 	for body in bodies:
 		if body.is_in_group("Bricks"):
 			body.queue_free()
+		
+		if body.get_name() == "Paddle":
+			var speed = get_linear_velocity().length()
+			var direction = position - body.get_node("Anchor").get_global_position()
+			var velocity = direction.normalized() * min(speed + SPEEDUP*delta, MAXSPEED*delta)
+			set_linear_velocity(velocity)
+
+	if position.y > get_viewport_rect().end.y:
+		print("Ball died.")
+		queue_free()
