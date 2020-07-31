5f61031de1090938d1f301d4aa6985945c83ea72
Soulzerz <Soulzerz@github.com>
2020-07-31 16:53:28 -0400

    Modified: Ball and Brick nodes into scenes for easier instancing.

--- Scripts/Paddle.gd
+++ Scripts/Paddle.gd
@@ -1,18 +1,20 @@
 extends KinematicBody2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+const  BALL_SCENE = preload("res://Mini Scenes/Ball.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 	set_physics_process(true)
+	set_process_input(true)
 
 func _physics_process(delta):
 	var y = position.y
 	var mouse_x = get_viewport().get_mouse_position().x
 	position = Vector2(mouse_x, y)
 	
+func _input(event):
+	if event is InputEventMouseButton && event.is_pressed():
+		var ball = BALL_SCENE.instance()
+		ball.position = ball.position - Vector2(0, 16)
+		get_tree().get_root().add_child(ball)
